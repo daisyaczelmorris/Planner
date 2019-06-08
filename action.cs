8a1f70256be1ef98f522da44ab7e386e51342ea1
daisyaczelmorris <48541709+daisyaczelmorris@users.noreply.github.com>
2019-06-08 15:47:10 +1000

    Add files via upload

---
+++ action.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    abstract class Action
+
+    {
+        private string _id;
+        private bool _result;
+        
+        public Action(string id) {
+            _id = id;
+        }
+        public abstract bool Execute();
+        public string Id
+        {
+            get { return _id; }
+        }
+
+        public bool Result { get => _result; set => _result = value; }
+    }
+}
